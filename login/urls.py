d9d187e10e39666e141ae92c1bba8a964cf5dee3
Wladimir Arias <wladimir.arias.r@gmail.com>
2022-12-01 09:01:08 -0300

    .

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from login.views import *
 from app.views import *
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns = [
     path("inicio/", iniciar_sesion, name="auth-login"),
     path("logout/", cerrar_sesion, name="auth-logout")
